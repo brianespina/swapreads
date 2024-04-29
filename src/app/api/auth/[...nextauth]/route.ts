c663f0f70223179e9f7de36c715cfaf1b7930f18
Louis Moses <louismoses.rait@gmail.com>
2024-04-30 00:44:54 +0800

    add google auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const providers = [
+  GithubProvider({
+    clientId: process.env.GITHUB_CLIENTID,
+    clientSecret: process.env.GITHUB_CLIENTSECRET,
+  }),
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    authorization: {
+      params: {
+        prompt: "consent",
+        access_type: "offline",
+        response_type: "code",
+      },
+    },
+  }),
+];
+
+const handler = NextAuth({
+  secret: "this is my secret",
+  providers,
+});
+
+export { handler as GET, handler as POST };
