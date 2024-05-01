4d121694f5101b4892ac4a59636f117281f41271
Louis Moses <louismoses.rait@gmail.com>
2024-05-01 16:16:32 +0800

    create a signup page

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,25 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import EmailProvider from "next-auth/providers/email";
 
 const providers = [
+  CredentialsProvider({
+    credentials: {
+      username: { label: "Username", type: "text" },
+      password: { label: "Password", type: "password" },
+    },
+    async authorize(credentials, req) {
+      const { username, password } = credentials as {
+        username: string;
+        password: string;
+      };
+
+      return userService.authenticate(username, password); //(5)
+    },
+  }),
+
   GithubProvider({
     clientId: process.env.GITHUB_CLIENTID,
     clientSecret: process.env.GITHUB_CLIENTSECRET,
