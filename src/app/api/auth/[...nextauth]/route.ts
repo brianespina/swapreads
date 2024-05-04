76da3f0a0e7720cb9cad8596188305463c4b0d6b
Brian Espina <espinabrian@gmail.com>
2024-05-04 21:47:51 +0800

    creacted login page, removed github provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,39 +4,32 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const providers = [
-  CredentialsProvider({
-    credentials: {
-      username: { label: "Email", type: "text" },
-      password: { label: "Password", type: "password" },
-    },
-    async authorize(credentials, req) {
-      const { email, password } = credentials as {
-        username: string;
-        password: string;
-      };
-    },
-  }),
+    CredentialsProvider({
+        credentials: {
+            username: { label: "Email", type: "text" },
+            password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials):Promise<any> {
+            console.log(credentials)
+        },
+    }),
 
-  GithubProvider({
-    clientId: process.env.GITHUB_CLIENTID,
-    clientSecret: process.env.GITHUB_CLIENTSECRET,
-  }),
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    authorization: {
-      params: {
-        prompt: "consent",
-        access_type: "offline",
-        response_type: "code",
-      },
-    },
-  }),
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        authorization: {
+            params: {
+                prompt: "consent",
+                access_type: "offline",
+                response_type: "code",
+            },
+        },
+    }),
 ];
 
 const handler = NextAuth({
-  secret: "this is my secret",
-  providers,
+    secret: "this is my secret",
+    providers,
 });
 
 export { handler as GET, handler as POST };
---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import Form from "./form"
+
+export default function() {
+    return (
+        <div className="page w-full h-screen bg-brown-50 flex justify-center items-center">
+            <div className="signup ">
+                <div className="card">
+                    <Form />
+                </div>
+            </div>
+        </div>
+    )
+}
