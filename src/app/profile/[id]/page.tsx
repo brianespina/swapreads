726e5b3b1276c3bd139d8fe197f26f74efa707f3
Brian Espina <espinabrian@gmail.com>
2024-05-12 20:10:36 +0800

    added add book drawer

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,5 +1,15 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { Button } from "@/components/ui/button"
 
 export default async function Page({ params }: { params: { id: string } }) {
     const session = await getServerSession(authOptions)
@@ -7,6 +17,19 @@ export default async function Page({ params }: { params: { id: string } }) {
         return (
             <div>redirect to login page</div>
         )
-    return <div>show page {params.id}</div>
+    return (<div>
+        <Sheet>
+            <SheetTrigger asChild><Button>Add Book</Button></SheetTrigger>
+            <SheetContent side="left">
+                <SheetHeader>
+                    <SheetTitle>Book Form</SheetTitle>
+                    <SheetDescription>
+                        Book Form Details here
+                    </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+
+    </div>)
 }
 
