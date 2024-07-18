006f49abbd6713ad78d04ac7668d8d9c92686312
Louis Moses <louismoses.rait@gmail.com>
2024-07-18 20:33:19 +0800

    feat(carousel): add trending book carousel

--- src/app/home/TrendingBooks.tsx
+++ src/app/home/TrendingBooks.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const TrendingBook = () => {
   const [trendingBooks, setTrendingBooks] = useState([]);
@@ -20,35 +24,76 @@ const TrendingBook = () => {
       console.log(error);
     }
   }
+  //
   useEffect(() => {
     getTrendingBooks();
   }, []);
+  //
+
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    rows: 2,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    arrows: true,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="relative">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+    <div className="relative pb-[80px]">
+      <Slider {...settings}>
         {trendingBooks.map((book, index) => (
-          <div
-            className="text-center items-center flex flex-col  border rounded-md shadow-lg hover:scale-105 hover:border-neutral-400"
-            key={index}
-          >
-            <div className="flex justify-center items-cente h-[320px] relative max-w-full">
-              <Image
-                src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg?default=false`}
-                width={600}
-                height={240}
-                className="object-cover rounded-t-md"
-                alt={book.title}
-              />
-            </div>
-            <div className="p-4 flex flex-col items-center justify-center h-full">
-              <h4 className="text-lg font-bold leading-tight">{book.title}</h4>
-              <h4 className="text-neutral-400 leading-tight">
-                {book.author_name}
-              </h4>
+          <div key={index}>
+            <div className="m-4 shadow-lg ">
+              <div className="flex justify-center items-cente h-[320px] relative max-w-full">
+                <Image
+                  src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg?default=false`}
+                  width={600}
+                  height={240}
+                  className="object-cover rounded-t-md"
+                  alt={book.title}
+                />
+              </div>
+              <div className="p-4 flex flex-col items-center justify-center h-full text-center">
+                <h4 className="text-lg font-bold leading-tight">
+                  {book.title}
+                </h4>
+                <h4 className="text-neutral-400 leading-tight">
+                  {book.author_name}
+                </h4>
+              </div>
             </div>
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
